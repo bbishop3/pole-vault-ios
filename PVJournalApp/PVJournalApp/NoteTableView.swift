f7b53275a50eed8ed3a3ed1e435c969aa98275e9
Brittany Bishop <bbishop3@samford.edu>
2023-03-15 14:17:00 -0500

    Next step is to code save data

---
+++ PVJournalApp/PVJournalApp/NoteTableView.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class NoteTableView: UITableViewController{
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }//viewDidLoad func
+    
+    
+}//NoteTableView class
---
+++ PVJournalApp/PVJournalApp/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleTF: UITextField!
+    
+    @IBOutlet weak var descTV: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func saveAction(_ sender: Any) {
+    }
+    
+}
+
